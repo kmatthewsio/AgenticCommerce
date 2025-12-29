0e5c94a3bed0f05d6e50e71f048568ea378b786c
kmatthewsio <2Krypto2Kwit@pm.me>
2025-12-29 08:34:02 -0500

    Add project files.

---
+++ src/AgenticCommerce.Core/Interfaces/IArcClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgenticCommerce.Core.Interfaces
+
+{
+    /// <summary>
+    /// Defines the contract for an ARC client that provides access to ARC-related operations and services.
+    /// </summary>
+    /// <remarks>Implement this interface to create a client capable of interacting with ARC resources. The
+    /// specific operations and usage details are defined by the implementing class.</remarks>
+    public interface  IArcClient
+    {
+        /// <summary>
+        /// Check if connected arc
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> IsConnectedAsync();
+    }
+}
