4cc6dd0d06d2aa33692db558054a1a85fccfbd10
kmatthewsio <2Krypto2Kwit@pm.me>
2026-01-06 14:03:14 -0500

    deployed to PROD

--- src/AgenticCommerce.API/Program.cs
+++ src/AgenticCommerce.API/Program.cs
@@ -64,14 +64,14 @@ builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "AgenticCommerce.API v1");
     });
-}
+//}
 
 app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
